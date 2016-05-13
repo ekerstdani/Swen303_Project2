b5e8b111931fdbae7f08c7123a218f8e00e81b45
Denpa4u <Denpa4u@gmail.com>
2016-05-13 21:40:45 +1200

    UI: Basic homepage

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var websiteName = 'Website Name';
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index', { title: websiteName });
 });
 
 module.exports = router;

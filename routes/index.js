35b96916b2b0034b4dca2eda6c2a28d3265a85f0
Denpa4u <Denpa4u@gmail.com>
2016-05-13 22:31:55 +1200

    UI: Product & Sign In pages

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,17 @@ router.get('/', function(req, res) {
   res.render('index', { title: websiteName });
 });
 
+router.get('/product', function(req, res) {
+  var exampleProduct = {};
+  exampleProduct.title = "Test Product";
+  exampleProduct.price = "$13.37";
+  exampleProduct.description = "This is a test product. Dank af.";
+
+  res.render('product', { title: websiteName, product: exampleProduct });
+});
+
+router.get('/login', function(req, res) {
+  res.render('login', { title: websiteName });
+});
+
 module.exports = router;
